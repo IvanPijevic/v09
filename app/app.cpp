4ce013e1329b92305040b04972a9be83b59a9c59
Asfodel <ivan.pijevic@gmail.com>
2025-01-21 11:57:28 +0100

    v09

--- app/app.cpp
+++ app/app.cpp
@@ -2,5 +2,69 @@
 
 namespace vsite::oop::v9
 {
+	void fill_vector(std::vector<int>& vec, int vectorSize)
+	{
+		for (int i = 0; i < vectorSize; ++i)
+		{
+			vec.push_back(i * i);
+		}
+	}
 
+	void remove_element(std::vector<int>& vec, int index) 
+	{
+		vec.erase(vec.begin() + index);
+	}
+
+	void input_element(std::vector<std::string>& vec, int index, const std::string& str) 
+	{
+		vec.insert(vec.begin() + index, str);
+	}
+
+	int list_nth_element(std::list<int>& lst, int index) 
+	{
+		std::list<int>::iterator iter = lst.begin();
+		std::advance(iter, index);
+		return *iter;
+	}
+
+	void list_sort_desc(std::list<int>& lst)
+	{
+		lst.sort(std::greater<int>());
+	}
+
+	uint32_t unique_numbers(std::istream& is) 
+	{
+		std::set<int> numbers;
+		int num = 0;
+
+		while (is >> num) 
+		{
+			numbers.insert(num);
+		}
+
+		return numbers.size();
+	}
+
+	word_frequency::word_frequency(std::istream& is) 
+	{
+		std::string word;
+		while (is >> word)
+		{
+			for (char& letter : word) 
+			{
+				letter = tolower(letter);
+			}
+			m_map[word]++;
+		}
+	}
+
+	uint32_t word_frequency::count() const
+	{
+		return m_map.size();
+	}
+
+	uint32_t word_frequency::frequency(const std::string& str) const
+	{
+		return m_map.find(str) != m_map.end() ? m_map.at(str) : 0;
+	}
 }
--- app/app.h
+++ app/app.h
@@ -1,5 +1,32 @@
 #pragma once
+#include <vector>
+#include <string>
+#include <list>
+#include <map>
+#include <set>
+#include <sstream>
 
 namespace vsite::oop::v9 
 {
+	void fill_vector(std::vector<int>& vec, int vectorSize);
+	void remove_element(std::vector<int>& vec, int index);
+	void input_element(std::vector<std::string>& vec, int index, const std::string& str);
+
+	int list_nth_element(std::list<int>& lst, int index);
+
+	void list_sort_desc(std::list<int>& lst);
+
+	uint32_t unique_numbers(std::istream& is);
+
+	class word_frequency
+	{
+	public:
+		word_frequency(std::istream& is);
+		uint32_t count() const;
+		uint32_t frequency(const std::string& str) const;
+
+	private:
+		std::map<std::string, int> m_map;
+	};
+
 }
